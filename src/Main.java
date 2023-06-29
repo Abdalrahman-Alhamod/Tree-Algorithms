d5c906828112d690543dd9b2b2b2dd052b88bc94
Abd-HM <abd.alrrahman.alhamod@gmail.com>
2023-06-30 00:56:54 +0300

    Retesting

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         AVL<Integer> tree = new AVL<>();
-        tree.insert(2);
-        tree.insert(7);
         tree.insert(1);
+        tree.insert(2);
         tree.insert(3);
+        tree.insert(4);
         tree.insert(5);
+        tree.insert(6);
+        tree.insert(7);
         tree.insert(8);
         tree.insert(9);
-        tree.insert(10);
         System.out.println(tree);
     }
 }
